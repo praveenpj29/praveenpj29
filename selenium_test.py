a25c53685d34f6db5eab88c2d147782f20a68c9f
praveenpj29 <54732830+praveenpj29@users.noreply.github.com>
2020-08-24 22:26:35 +0530

    updated

--- selenium_test.py
+++ selenium_test.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 import requests
-import openpyxl
 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
@@ -12,12 +11,22 @@ if __name__ == "__main__":
     header_id = "//*[@id='nav-xshop']/"
     header = driver.find_elements(by=By.XPATH, value=header_id + "a")
     time.sleep(1)
-    header_names = [i.get_attribute for i in header]
+    header_names = [i.get_attribute("text") for i in header]
     xpath = [header_id + 'a[{}]'.format(i) for i in range(1, len(header) + 1)]
     url = [i.get_attribute("href") for i in header]
-    request = [requests.get(i).status_code for i in url]
-    remarks = ['passed' if i == 200 else 'failed' for i in request]
-    df = {'header_names': header_names, 'x_path': xpath, 'url': url, 'request': request, 'remarks': remarks}
+    validation = []
+    counter = 0
+    for i in xpath:
+        driver.find_element(by=By.XPATH, value=i).click()
+        time.sleep(3)
+        if driver.current_url == url[counter]:
+            validation.append("Passed")
+        else:
+            validation.append("Failed")
+        driver.back()
+        time.sleep(3)
+        counter += 1
+    df = {'header_names': header_names, 'x_path': xpath, 'url': url, 'validation': validation}
     df = pd.DataFrame(df)
     excel = pd.ExcelWriter("output.xlsx")
     df.to_excel(excel)
